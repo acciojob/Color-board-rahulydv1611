4d83c5315d5e019c0a50c00538eb90ff7c93872c
Rahul Yadav <130846443+rahulydv1611@users.noreply.github.com>
2026-03-28 00:10:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
 //your JS code here. If required.
+const container = document.getElementById("container");
+
+// create 800 boxes
+for (let i = 0; i < 800; i++) {
+    const square = document.createElement("div");
+    square.classList.add("square");
+
+    square.addEventListener("mouseover", () => setColor(square));
+    square.addEventListener("mouseout", () => removeColor(square));
+
+    container.appendChild(square);
+}
+
+// random color generator
+function getRandomColor() {
+    const colors = ["#e74c3c", "#8e44ad", "#3498db", "#e67e22", "#2ecc71"];
+    return colors[Math.floor(Math.random() * colors.length)];
+}
+
+// apply color
+function setColor(el) {
+    const color = getRandomColor();
+    el.style.backgroundColor = color;
+}
+
+// remove color smoothly
+function removeColor(el) {
+    setTimeout(() => {
+        el.style.backgroundColor = "#1d1d1d";
+    }, 1000);
+}
